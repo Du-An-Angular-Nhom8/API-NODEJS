f30b0b99f0ea79d952d5af49aad95fafc383086c
quan1234 <quanvdph19781@fpt.edu.vn>
2023-06-04 03:35:46 +0700

    Bug

--- src/router/auth.js
+++ src/router/auth.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { GetOneUser, signin, } from "../controllers/auth";
 import { getAllUser, singup } from "../controllers/auth";
+import { signin, singup } from "../controllers/auth";
+import { getAllUser } from "../controllers/auth";
 
 
 const router = express.Router()
