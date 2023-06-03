6ecaf4b9a25d0c45e69a3bae5a3cacb3b64f2751
Đào Xuân Mai - FE <118628374+mailuoicode@users.noreply.github.com>
2023-06-03 23:19:41 +0700

    create check auth --check auth: product, category crud refs:WA-29

--- src/router/product.js
+++ src/router/product.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { CreateProduct, RemoveProduct, getAll, getOne, update } from "../controllers/product";
+import { checkPermission } from "../middleware/checkPermisson";
 
 const router = express.Router()
 
-router.post('/product/add', CreateProduct),
-    router.delete('/product/:id', RemoveProduct),
+router.post('/product/add', checkPermission, CreateProduct),
+    router.delete('/product/:id', checkPermission, RemoveProduct),
     router.get('/product', getAll)
-router.put("/product/:id/edit", update)
+router.put("/product/:id/edit", checkPermission, update)
 router.get("/product/:id", getOne)
 export default router
