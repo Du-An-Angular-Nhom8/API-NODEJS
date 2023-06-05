20c70a0fb675f1b70b6e2e30a34338e358a87ee0
Đào Xuân Mai - FE <118628374+mailuoicode@users.noreply.github.com>
2023-06-05 17:00:38 +0700

    fix errror file router auth

--- src/router/auth.js
+++ src/router/auth.js
@@ -1,8 +1,8 @@
 import express from "express";
 import { GetOneUser, signin, } from "../controllers/auth";
 import { getAllUser, singup } from "../controllers/auth";
-import { signin, singup } from "../controllers/auth";
-import { getAllUser } from "../controllers/auth";
+// import { signin, singup } from "../controllers/auth";
+// import { getAllUser } from "../controllers/auth";s
 
 
 const router = express.Router()
