88442f0d72cb6359ac52a676f1fcdaa12bf50ada
quan1234 <quanvdph19781@fpt.edu.vn>
2023-06-10 17:33:11 +0700

    add new price cart

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -4,6 +4,7 @@ import jwt from "jsonwebtoken"
 // Controller để thêm sản phẩm vào cart của người dùng khi có token
 export const addToCart = async (req, res) => {
     try {
+        const price = req.body.price
         // Xác thực token và lấy thông tin người dùng
         const token = req.headers.authorization?.split(" ")[1];
         if (!token) {
@@ -15,12 +16,16 @@ export const addToCart = async (req, res) => {
         // Kiểm tra xem người dùng đã có cart hay chưa
         let user = await User.findById(userId);
         let cart;
-        if (!user.cart) {
+       
+        if (!user.cart) { 
+            
             // Nếu người dùng chưa có cart, tạo một cart mới
-            cart = new Cart({ userId, products: [] });
+            cart = new Cart({ userId, price, products: [] });
+           
             user.cart = cart._id;
             await user.save();
         } else {
+            console.log("okok");
             // Nếu người dùng đã có cart, lấy cart hiện có
             cart = await Cart.findById(user.cart);
             //   await cart.save()
@@ -29,12 +34,15 @@ export const addToCart = async (req, res) => {
         // Thêm sản phẩm vào cart
 
         const productId = req.body._id;
-
+        console.log(productId);
+        // const data = { productId, price }
         cart.products.push(productId);
         // console.log(cart);
+        // console.log(price);
         await cart.save();
 
-        return res.status(200).json({ message: 'Sản phẩm đã được thêm vào cart' });
+
+        return res.status(200).json({ message: 'Sản phẩm đã được thêm vào cart', cart });
     } catch (err) {
         return res.status(500).json({ error: 'Đã xảy ra lỗi khi thêm sản phẩm vào cart' });
     }
@@ -69,6 +77,7 @@ export const removeFromCart = async (req, res) => {
         if (!token) {
             throw new Error("Bạn chưa đăng nhập");
         }
+
         const decoded = jwt.verify(token, "boquan");
         const userId = decoded._id;
 
